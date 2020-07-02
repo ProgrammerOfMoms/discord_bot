60f313a51e2981359ba6372ce31d98511b35b94f
admin <admin@LAPTOP-98V9G3FJ>
2020-07-02 13:38:06 +0900

    add static and media dirs

--- BotAdmin/BotAdmin/settings.py
+++ BotAdmin/BotAdmin/settings.py
@@ -84,21 +84,12 @@ WSGI_APPLICATION = 'BotAdmin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if DEBUG:
-
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'dbp.sqlite3'),
-        }
-    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
